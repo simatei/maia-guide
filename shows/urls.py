4897f9717cf0c55d2d075df08cb6bee9c4108e5e
simatei <dr.robsimatei@gmail.com>
2019-08-03 18:33:08 +0300

    Add urls for shows

--- shows/urls.py
+++ shows/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home_view, name='home'),
+    path('movies/', views.available_movies, name='movies'),
+    path('shows/', views.available_shows, name='shows')
 ]
