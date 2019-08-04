5cee26425cdf7136d28b86f475cb6e5cf8a63ded
simatei <dr.robsimatei@gmail.com>
2019-08-04 16:37:17 +0300

    Add test for /focus/ url

--- shows/tests.py
+++ shows/tests.py
@@ -13,3 +13,7 @@ class TestUrls(TestCase):
     def test_shows(self):
         response = self.client.get('/shows/')
         self.assertEqual(response.status_code, 200)
+
+    def test_play_trailer(self):
+        response = self.client.get('/focus/157336/') # Hardcode videoId for movie 'Interstellar'
+        self.assertEqual(response.status_code, 200)
